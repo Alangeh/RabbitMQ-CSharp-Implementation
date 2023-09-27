987500f0aff4017f717fc2341eee97b9eeb3ef68
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-09-27 21:06:21 +0300

    coding the producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ 
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox", 
+    durable:false, 
+    exclusive:false,
+    autoDelete:false,
+    arguments: null
+    );
+
+var message = "First message of RABBITMQ using c#";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
