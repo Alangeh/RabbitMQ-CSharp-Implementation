f0d2a454a7717cbe4033ae53d72f2915822c3edf
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-02 14:39:27 +0300

    Writing producer, 1st and 2nd consumer for pubsub

--- PubSub/ProducerPubSub/Program.cs
+++ PubSub/ProducerPubSub/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+var message = "Pub Sub braodcast messge";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "pubsub", "", null, body);
+
+Console.WriteLine($"Send message: {message}");
--- PubSub/SecondConsumer/Program.cs
+++ PubSub/SecondConsumer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.QueueBind(queue: queueName, exchange: "pubsub", routingKey: "");
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Second Consumer Received new message: {message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+
+Console.ReadKey();
