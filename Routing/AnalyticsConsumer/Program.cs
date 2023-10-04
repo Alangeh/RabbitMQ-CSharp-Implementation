0645c81c6bffd2b18a136c5adfd6bc055285b386
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-04 22:02:09 +0300

    Setting up boiler plate for direct routing

---
+++ Routing/AnalyticsConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "routing", type: ExchangeType.Direct);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.QueueBind(queue: queueName, exchange: "routing", routingKey: "analyticskey");
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Analytics Consumer Received new message: {message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+
+Console.ReadKey();
---
+++ Routing/Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "routing", type: ExchangeType.Direct);
+
+var message = "Direct routing braodcast messge";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "pubsub", "analyticskey", null, body);
+
+Console.WriteLine($"Send message: {message}");
